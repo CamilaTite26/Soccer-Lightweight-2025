a94df21c70701e07ef71d56b8b9911037f286ca1
Camila Tite <a00841255@tec.mx>
2025-02-13 19:49:40 -0600

    Changes in Motor cpp

--- Motor.cpp
+++ Motor.cpp
@@ -1,40 +1,39 @@
 #include "Arduino.h"
-#include "Motor.h"  
+#include "Motor.h"
 
-motor::motor(){
+motor::motor() {
     pin1_ = 0;
     pin2_ = 0;
     speed_ = 0;
+}
 
-};
-
-motor::motor(int pin1, int pin2, int speed){
+motor::motor(int pin1, int pin2, int speed) {
     pin1_ = pin1;
     pin2_ = pin2;
     speed_ = speed;
-};
+}
 
-void startMotors(){
+void motor::startMotors() {
     pinMode(pin1_, OUTPUT);
     pinMode(pin2_, OUTPUT);
     pinMode(speed_, OUTPUT);
+}
 
-};
-void Motor::SetSpeed(int pwm, int speed){
-    analogWrite(pwm, speed);
-};
-void motor::forward(){
+void motor::SetSpeed(int speed) {
+    analogWrite(speed_, speed);
+}
+
+void motor::forward() {
     digitalWrite(pin1_, HIGH);
     digitalWrite(pin2_, LOW);
+}
 
-};
-void motor::backward(){
+void motor::backward() {
     digitalWrite(pin1_, LOW);
     digitalWrite(pin2_, HIGH);
+}
 
-};
-void motor::stop(){
+void motor::stop() {
     digitalWrite(pin1_, LOW);
     digitalWrite(pin2_, LOW);
-
-};
+}
