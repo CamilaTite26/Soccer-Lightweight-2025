177fa65ea122df8d89704af3543c29ae180e2746
bahena <a01199088tec.mx>
2025-02-21 16:24:26 -0600

    Bno functions and test

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,27 @@
-#include "motors.h"
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <Bno.h>
 
-
-Motors motors(
-    MOTOR1_PWM, MOTOR1_IN1, MOTOR1_IN2, 
-    MOTOR2_PWM, MOTOR2_IN1, MOTOR2_IN2, 
-    MOTOR3_PWM, MOTOR3_IN1, MOTOR3_IN2, 
-    MOTOR4_PWM, MOTOR4_IN1, MOTOR4_IN2
-);
-
-void setup() {
-    Serial.begin(9600);
-    motors.InitializeMotors();  // Inicializar los motores
-    Serial.println("Prueba de motores iniciada.");
+Bno bno;
+void setup(void) 
+{
+  Serial.begin(115200);
+  bno.bno_begin();
 }
 
-void loop() {
-    Serial.println("Mover hacia adelante");
-    motors.SetAllSpeeds(90);
-    motors.MoveForward();
-    /*
-    Serial.println("Mover hacia atrás");
-    motors.MoveBackward();
-    delay(2000);
+void loop(void) 
+{
+  /* Get a new sensor event */ 
+  // Possible vector values can be:
+  // - VECTOR_ACCELEROMETER - m/s^2
+  // - VECTOR_MAGNETOMETER  - uT
+  // - VECTOR_GYROSCOPE     - rad/s
+  // - VECTOR_EULER         - degrees
+  // - VECTOR_LINEARACCEL   - m/s^2
+  // - VECTOR_GRAVITY       - m/s^2
+  bno.getEuler();
 
-    Serial.println("Girar a la izquierda");
-    motors.MoveLeft();
-    delay(2000);
-
-    Serial.println("Girar a la derecha");
-    motors.MoveRight();
-    delay(2000);
-
-    Serial.println("Girar en círculos");
-    for (int angle = 0; angle < 360; angle += 45) {  // Gira en múltiples ángulos
-        motors.MoveMotors(angle, 255);
-        delay(500);
-    }
-
-    Serial.println("Detener motores");
-    motors.StopMotors();
-    delay(3000);  
-    */
+  delay(100);
 }
-
